c15bb2f41762aa8894828128b433774a062b359c
tenminuteversity <kibria7533>
2022-06-14 08:04:51 +0600

    payment url added

--- src/Url/Url.js
+++ src/Url/Url.js
@@ -1,7 +1,7 @@
 const ORDER_SERVICE='https://rabbitmq-order-service.herokuapp.com';
 const USER_SERVICE='https://rabbitmq-user-service.herokuapp.com';
 const STOCK_SERVICE='https://rabbitmq-stock-service.herokuapp.com';
-const PAYMENT_SERVICE='https://dashboard.heroku.com/apps/rabbitmq-payment-service';
+const PAYMENT_SERVICE='https://rabbitmq-payment-service.herokuapp.com';
 
 module.exports = {
     ORDER_SERVICE,USER_SERVICE,STOCK_SERVICE,PAYMENT_SERVICE
